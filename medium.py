98a0670659199c59d5838f8e07e2afc58be4e68a
hkad98 <kadlino98@seznam.cz>
2022-12-01 11:11:23 +0100

    Add publication + requirements

--- medium.py
+++ medium.py
@@ -74,6 +74,7 @@ class StatGrabberUser(StatGrabberBase):
     u = StatGrabberUser("","","",datetime(year=2020, month=1, day=1), datetime.now())
     u.get_summary_stats()
     """
+
     def __init__(self, username: str, sid: str, uid: str, start: datetime, stop: datetime):
 
         self.username = str(username)
@@ -97,6 +98,67 @@ class StatGrabberUser(StatGrabberBase):
         if data.get("paging", {}).get("next"):
             next_cursor_idx = data["paging"]["next"]["to"]
             next_page = self.get_summary_stats(limit=limit, to=next_cursor_idx)
+            data["value"].extend(next_page)
+
+        return data["value"]
+
+
+class StatGrabberPublication(StatGrabberBase):
+    def __init__(self, slug, sid, uid, start, stop):
+
+        url = f"https://medium.com/{slug}"
+        self.url = url
+        super().__init__(sid, uid, start, stop)
+        homepage = self._fetch(self.url)
+        # TODO figure out why requests lib doesn't get full html from this url
+        data = self._decode_json(homepage)
+        self.attrs_json = data["collection"]
+        self._unpack_attrs(self.attrs_json)
+
+        collections_endpoint = f"https://medium.com/_/api/collections/{self.id}/stats/"
+        timeframe = f"?from={self.start_unix}&to={self.stop_unix}"
+        create_endpoint = lambda x: collections_endpoint + x + timeframe
+        self.views_endpoint = create_endpoint("views")
+        self.visitors_endpoint = create_endpoint("visitors")
+
+    # TODO - create a helper classmethod that takes in a URL and extracts slug
+
+    def _unpack_attrs(self, attrs_json):
+
+        self.id = self.attrs_json["id"]
+        self.slug = self.attrs_json["slug"]
+        self.name = self.attrs_json["name"]
+        self.creator = self.attrs_json["creatorId"]
+        self.description = self.attrs_json["description"]
+        try:
+            self.domain = self.attrs_json["domain"]
+        except:
+            self.domain = None
+
+    def __repr__(self):
+        return f"{self.name} - {self.description}"
+
+    def get_events(self, type_="views"):
+        if type_ == "views":
+            response = self._fetch(self.views_endpoint)
+        elif type_ == "visitors":
+            response = self._fetch(self.visitors_endpoint)
+        else:
+            raise ValueError('"type_" param must be either "views" or "visitors"')
+
+        data = self._decode_json(response)
+
+        return data["value"]
+
+    def get_all_story_overview(self, limit=50, **kwargs):
+        params = {"limit": limit, **kwargs}
+        endpoint = f"https://medium.com/{self.slug}/stats/stories"
+        response = self._fetch(endpoint, params)
+
+        data = self._decode_json(response)
+        if data.get("paging", {}).get("next"):
+            next_cursor_idx = data["paging"]["next"]["to"]
+            next_page = self.get_all_story_overview(limit=limit, to=next_cursor_idx)
             data["value"].extend(next_page)
 
         return data["value"]
